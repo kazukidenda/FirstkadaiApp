453249c6d1fb629f2e9f4e1dd34c0990460252c8
kazuki 9425 <lexusrx2017@yahoo.co.jp>
2021-02-04 16:07:12 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 傳田和樹 on 2021/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
